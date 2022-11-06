c2cb7bb931eeb5224d55fa26b468c6c6af8733f3
Marshflair1988 <104839692+Marshflair1988@users.noreply.github.com>
2022-11-06 23:38:30 +0100

    Javascript module 3 training

---
+++ js/script.js
@@ -0,0 +1,59 @@
+
+// Question 1
+
+const getRemainder = (a, b) => a % b;
+   
+    
+const result = getRemainder(15, 10);
+
+
+console.log(result);
+
+
+
+
+// Question 2
+
+const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=5f3b1807d5b64e5e84feeba8476b6ea2";
+
+const loaderAnimation = document.querySelector(".loader")
+const resultsContainer = document.querySelector(".container")
+
+async function getGames() {
+
+    try {
+        const response = await fetch(url);
+
+        const data = await response.json();
+
+        console.log(data);
+
+        const facts = data.results;
+        const ratings = data.rating;
+        const numberTags = data.tags
+    
+        resultsContainer.innerHTML = "";
+
+    for(let i = 0; i < facts.length; i++)  {
+        console.log(facts[i]);
+
+        if (i === 8) {
+            break;
+        }
+
+        const tagsLength = facts[i].tags.length;
+
+        loaderAnimation.innerHTML = "";
+        resultsContainer.innerHTML += `<div class="result">
+        <span><li>Title: ${facts[i].name}</li></span>
+        <span><li>Rating: ${facts[i].rating}</li></span>
+        <span><li>Tags: ${tagsLength}</li></span>
+        </div>`;
+    }
+    } catch (error){
+        resultsContainer.innerHTML = displayError("An error occured when calling the API");
+    }
+    
+}
+
+getGames();
